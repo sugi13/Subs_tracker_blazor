597275207d327d7a510d0a4205fc8e5e3afae1d3
Sukant <sukantsivaraja2002@gmail.com>
2026-02-06 17:47:52 +0530

    added new component

--- models/Subscription.cs
+++ models/Subscription.cs
@@ -1,4 +1,4 @@
-
+using System.ComponentModel.DataAnnotations;
 
 namespace Subscription_tracker.models
 {
@@ -6,12 +6,16 @@ namespace Subscription_tracker.models
     {
         public int ItemId { get; set; }
         
+        [Required(ErrorMessage = "Amount required")]
+        [Range(1, 100000, ErrorMessage = "Amount must be greater than 0")]
         public int Subs_amount { get; set; }
         
         public int Subs_total { get; set; }
         
+        [Required(ErrorMessage = "Subscription name required")]
         public required string Subs_name { get; set; }
         
+        [Required(ErrorMessage = "Select a plan")]
         public required string Subs_plan { get; set; }
     }
 }
